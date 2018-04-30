85908d436fa10dbc7e91fb26333d1f6d7dfc175b
Pavel Kirienko <pavel.kirienko@gmail.com>
2018-04-30 15:55:39 +0300

    Mock tests

--- test/mocks.hpp
+++ test/mocks.hpp
@@ -97,8 +97,24 @@ class FileMappedROMBackend : public kocherga::IROMBackend
     bool inject_failure_ = false;
 
 
+    void checkFileHealth() const
+    {
+        std::ifstream f(file_name_, std::ios::binary | std::ios::in | std::ios::ate);
+        if (!f)
+        {
+            throw std::runtime_error("The ROM mapping file could not be open");
+        }
+
+        if (f.tellg() != rom_size_)
+        {
+            throw std::runtime_error("Invalid size of the ROM mapping file");
+        }
+    }
+
     std::int16_t beginUpgrade() override
     {
+        checkFileHealth();
+
         if (upgrade_in_progress_)
         {
             throw BadUsageException("beginUpgrade() called twice");
@@ -110,6 +126,8 @@ class FileMappedROMBackend : public kocherga::IROMBackend
 
     std::int16_t endUpgrade(bool success) override
     {
+        checkFileHealth();
+
         if (success == inject_failure_)
         {
             throw BadUsageException("Unexpected success or failure");
@@ -136,7 +154,15 @@ class FileMappedROMBackend : public kocherga::IROMBackend
             throw BadUsageException("Upgrade is not in progress!");
         }
 
-        std::ofstream f(file_name_, std::ios::binary | std::ios::out);
+        if ((offset + size) > rom_size_)
+        {
+            size = std::uint16_t(rom_size_ - offset);
+        }
+
+        checkFileHealth();
+
+        // We need the in flag to prevent truncation
+        std::ofstream f(file_name_, std::ios::binary | std::ios::out | std::ios::in);
         if (f)
         {
             f.seekp(std::streamoff(offset));
@@ -174,12 +200,41 @@ public:
         inject_failure_ = true;
     }
 
+    void removeFailure()
+    {
+        inject_failure_ = false;
+    }
+
+    bool isSameImage(const void* reference, std::size_t reference_size) const
+    {
+        std::vector<std::uint8_t> buffer(reference_size, 0);
+        std::ifstream f(file_name_, std::ios::binary | std::ios::in);
+        if (f)
+        {
+            f.seekg(0);
+            f.read(reinterpret_cast<char*>(buffer.data()), std::streamsize(reference_size));
+        }
+        else
+        {
+            throw std::runtime_error("Could not open the ROM mapping file for verification");
+        }
+
+        return std::memcmp(reference, buffer.data(), reference_size) == 0;
+    }
+
     std::int16_t read(std::size_t offset, void* data, std::uint16_t size) const override
     {
         if (size > 32767)
         {
             throw BadUsageException("Size is too big");
         }
+
+        if ((offset + size) > rom_size_)
+        {
+            size = std::uint16_t(rom_size_ - offset);
+        }
+
+        checkFileHealth();
 
         std::ifstream f(file_name_, std::ios::binary | std::ios::in);
         if (f)
--- test/test_mocks.cpp
+++ test/test_mocks.cpp
@@ -31,6 +31,7 @@
 #include "catch.hpp"
 #include "mocks.hpp"
 #include <thread>
+#include <algorithm>
 
 
 TEST_CASE("Mocks-Platform")
@@ -68,4 +69,70 @@ TEST_CASE("Mocks-Platform")
         std::this_thread::sleep_for(std::chrono::microseconds(1000));
         REQUIRE(a < pl.getMonotonicUptime());
     }
+}
+
+
+TEST_CASE("Mocks-ROM")
+{
+    using namespace mocks;
+
+    FileMappedROMBackend back("rom.tmp", 1024);
+
+    {
+        const std::vector<std::uint8_t> ref(1024, 0xFF);
+        REQUIRE(back.isSameImage(ref.data(), ref.size()));
+
+        const std::vector<std::uint8_t> ref2(123, 0xFF);
+        REQUIRE(back.isSameImage(ref2.data(), ref2.size()));
+
+        const std::vector<std::uint8_t> ref3(123, 0xAA);
+        REQUIRE(!back.isSameImage(ref3.data(), ref3.size()));
+    }
+
+    auto& interface = static_cast<kocherga::IROMBackend&>(back);
+
+    std::array<std::uint8_t, 32768> buf1{};
+
+    // Reading
+    REQUIRE(32 == interface.read(0, buf1.data(), 32));
+    REQUIRE(std::all_of(buf1.begin(), buf1.begin() + 32, [](auto x) { return x == 0xFF; }));
+
+    REQUIRE(1024 == interface.read(0, buf1.data(), 1024));
+    REQUIRE(std::all_of(buf1.begin(), buf1.begin() + 1024, [](auto x) { return x == 0xFF; }));
+
+    REQUIRE(512 == interface.read(512, buf1.data(), 1024));
+    REQUIRE(std::all_of(buf1.begin(), buf1.begin() + 512, [](auto x) { return x == 0xFF; }));
+
+    REQUIRE(128 == interface.read(512, buf1.data(), 128));
+    REQUIRE(std::all_of(buf1.begin(), buf1.begin() + 1024, [](auto x) { return x == 0xFF; }));
+
+    REQUIRE(0 == interface.read(1024, buf1.data(), 1));
+
+    back.injectFailure();
+    REQUIRE(-1 == interface.read(0, buf1.data(), 1024));
+    back.removeFailure();
+
+    REQUIRE(1024 == interface.read(0, buf1.data(), 2000));
+    std::all_of(buf1.begin(), buf1.begin() + 1024, [](auto x) { return x == 0xFF; });
+    REQUIRE(buf1[1024] == 0);
+    REQUIRE(buf1[1025] == 0);
+    REQUIRE(buf1[1026] == 0);
+
+    REQUIRE_THROWS_AS(interface.read(0, buf1.data(), 32768), BadUsageException);
+
+    // Writing
+    REQUIRE_THROWS_AS(interface.write(0, buf1.data(), 123), BadUsageException);
+    REQUIRE_THROWS_AS(interface.endUpgrade(true), BadUsageException);
+    REQUIRE_THROWS_AS(interface.endUpgrade(false), BadUsageException);
+    REQUIRE(0 == interface.beginUpgrade());
+    buf1.fill(0xAA);
+    REQUIRE_THROWS_AS(interface.write(0, buf1.data(), 32768), BadUsageException);
+    REQUIRE(128 == interface.write(0, buf1.data(), 128));
+    REQUIRE(128 == interface.write(512, buf1.data(), 128));
+    REQUIRE(0 == interface.endUpgrade(true));
+
+    REQUIRE(1024 == interface.read(0, buf1.data(), 2000));
+    REQUIRE(std::all_of(buf1.begin() + 0,   buf1.begin() + 128, [](auto x) { return x == 0xAA; }));
+    REQUIRE(std::all_of(buf1.begin() + 128, buf1.begin() + 512, [](auto x) { return x == 0xFF; }));
+    REQUIRE(std::all_of(buf1.begin() + 512, buf1.begin() + 640, [](auto x) { return x == 0xAA; }));
 }
