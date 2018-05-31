341e0a1288eb4c0eda1b6aa65d0d570c1386eb12
Pavel Kirienko <pavel.kirienko@gmail.com>
2018-05-31 21:00:17 +0300

    Printf fix

--- kocherga.hpp
+++ kocherga.hpp
@@ -494,7 +494,7 @@ class BootloaderController final
             state_ = state_on_success;
             boot_delay_started_at_ =
                 platform_.getMonotonicUptime();     // This only makes sense if the new state is BootDelay
-            KOCHERGA_TRACE("App found; version %d.%d.%x, flags %u, built %u, %d bytes\n",
+            KOCHERGA_TRACE("App found; version %u.%u.%x, flags %u, built %u, %u bytes\n",
                            unsigned(appdesc->app_info.major_version),
                            unsigned(appdesc->app_info.minor_version),
                            unsigned(appdesc->app_info.vcs_commit),
