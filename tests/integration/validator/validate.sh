700789e34ff4e1ab8779e908c067bd2283e3be6d
Pavel Kirienko <pavel.kirienko@gmail.com>
2021-08-05 22:40:58 +0300

    Complete the integration test suite for serial port

---
+++ tests/integration/validator/validate.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o nounset
+
+function die()
+{
+    echo >&2 "FAILURE: $*"
+    exit 1
+}
+
+export SOFTWARE_PACKAGE_DIR=`dirname "$0"`
+
+python -m pip --disable-pip-version-check install yakut~=0.6 || die "Failed to install Yakut"
+
+yakut -v orchestrate "$SOFTWARE_PACKAGE_DIR/serial.orc.yaml"
+result=$?
+[[ $result == 222 ]] || die "Unexpected exit code: $result"
+echo "Exit code $result is valid, test passed"
