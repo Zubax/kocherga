902ca1f66d2bf59a8f6f839b4ef44c74f4f0cacf
Pavel Kirienko <pavel.kirienko@gmail.com>
2021-09-01 14:53:12 +0300

    Add integration test for UAVCAN/CAN

--- tests/integration/validator/validate.sh
+++ tests/integration/validator/validate.sh
@@ -12,7 +12,14 @@ export SOFTWARE_PACKAGE_DIR=$(dirname "$0")
 
 python -m pip --disable-pip-version-check install yakut~=0.6 || die "Failed to install Yakut"
 
+echo "TESTING: UAVCAN/serial"
 yakut -v orchestrate "$SOFTWARE_PACKAGE_DIR/serial.orc.yaml"
 result=$?
 [[ $result == 222 ]] || die "Unexpected exit code: $result"
+echo "Exit code $result is valid, test passed"
+
+echo "TESTING: UAVCAN/CAN"
+yakut -v orchestrate "$SOFTWARE_PACKAGE_DIR/can.orc.yaml"
+result=$?
+[[ $result == 223 ]] || die "Unexpected exit code: $result"
 echo "Exit code $result is valid, test passed"
