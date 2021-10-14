7af903b15335cbefb7a62b6263b8f71eef7315ea
Pavel Kirienko <pavel.kirienko@gmail.com>
2021-10-14 15:16:16 +0300

    Try anotehr approach with superheader

--- tests/unit/empty_translation_unit.cpp
+++ tests/unit/empty_translation_unit.cpp
@@ -2,3 +2,5 @@
 // that are not part of any translation unit. This empty C++ file is therefore added to host header fields with the
 // help of the "-include" compiler option (that forces inclusion of header files into translation units).
 // Find details in the build script and at https://community.sonarsource.com/t/analyzing-a-header-only-c-library/51468
+
+#include "generated_header_that_includes_all_kocherga_headers.hpp"
