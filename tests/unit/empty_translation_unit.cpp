8dbfeb5ab7032a6c5d8260bb4dcde2955713ad79
Pavel Kirienko <pavel.kirienko@gmail.com>
2021-10-14 14:32:04 +0300

    Add an empty translation unit

---
+++ tests/unit/empty_translation_unit.cpp
@@ -0,0 +1,4 @@
+// This file is needed to work around an apparent limitation of SonarCloud: it seems to be unable to analyze headers
+// that are not part of any translation unit. This empty C++ file is therefore added to host header fields with the
+// help of the "-include" compiler option (that forces inclusion of header files into translation units).
+// Find details in the build script and at https://community.sonarsource.com/t/analyzing-a-header-only-c-library/51468
