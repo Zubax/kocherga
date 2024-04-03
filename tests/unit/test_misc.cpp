332ba5a668deb0e00c2b36326b67ea5588bc61df
Kalyan Sriram <kalyan@coderkalyan.com>
2024-04-03 14:53:04 -0500

    tests: move crc32c from serial to shared

--- tests/unit/test_misc.cpp
+++ tests/unit/test_misc.cpp
@@ -58,6 +58,34 @@ TEST_CASE("CRC16-CCITT")
     REQUIRE(0x0000U == crc.get());
 }
 
+TEST_CASE("serial::CRC32C")
+{
+    kocherga::detail::CRC32C crc;
+    crc.update(static_cast<std::uint8_t>('1'));
+    crc.update(static_cast<std::uint8_t>('2'));
+    crc.update(static_cast<std::uint8_t>('3'));
+    crc.update(static_cast<std::uint8_t>('4'));
+    crc.update(static_cast<std::uint8_t>('5'));
+    crc.update(static_cast<std::uint8_t>('6'));
+    crc.update(static_cast<std::uint8_t>('7'));
+    crc.update(static_cast<std::uint8_t>('8'));
+    crc.update(static_cast<std::uint8_t>('9'));
+
+    REQUIRE(0xE306'9283UL == crc.get());
+    REQUIRE(crc.getBytes().at(0) == 0x83U);
+    REQUIRE(crc.getBytes().at(1) == 0x92U);
+    REQUIRE(crc.getBytes().at(2) == 0x06U);
+    REQUIRE(crc.getBytes().at(3) == 0xE3U);
+
+    REQUIRE(!crc.isResidueCorrect());
+    crc.update(0x83U);
+    crc.update(0x92U);
+    crc.update(0x06U);
+    crc.update(0xE3U);
+    REQUIRE(crc.isResidueCorrect());
+    REQUIRE(0xB798'B438UL == (~crc.get()));
+}
+
 TEST_CASE("VolatileStorage")
 {
     struct Data
